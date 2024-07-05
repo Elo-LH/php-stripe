82106b7f5056c3931d64c0bef659defe0bffa2b4
Eloïse Le Hellard <eloise.le_hellard@outlook.fr>
2024-07-05 11:00:04 +0200

    feat: add stripe via composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '839c5fe448d26497a263aaabc5ddc1f8da9d5c3e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '839c5fe448d26497a263aaabc5ddc1f8da9d5c3e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v15.0.0',
+            'version' => '15.0.0.0',
+            'reference' => '96f0dbe9c2b5365d716f86d92a27feecca2cbaee',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
