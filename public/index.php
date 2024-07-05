1f92c73d35c6d4d07b45f7cf705ed7d3434008b4
Eloïse Le Hellard <eloise.le_hellard@outlook.fr>
2024-07-05 11:32:35 +0200

    feat : dotenv and gitignore

--- public/index.php
+++ public/index.php
@@ -1,1 +1,5 @@
 <?php require "../app/views/checkout.html";
+
+// charge le contenu du .env dans $_ENV
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
--- public/js/checkout.js
+++ public/js/checkout.js
@@ -3,18 +3,16 @@
 /* global fetch */
 /* global URLSearchParams */
 
-const stripe;
+const stripe = $_ENV["SK_TEST"]
 
-let amount;
-initialize();
+let amount
+initialize()
 
-let elements;
+let elements
 
-checkStatus();
+checkStatus()
 
-document
-  .querySelector("#payment-form")
-  .addEventListener("submit", handleSubmit);
+document.querySelector("#payment-form").addEventListener("submit", handleSubmit)
 
 // Fetches a payment intent and captures the client secret
 async function initialize() {
@@ -22,32 +20,33 @@ async function initialize() {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ amount }),
-  }).then((r) => r.json());
+  }).then((r) => r.json())
 
-  elements = stripe.elements({ clientSecret });
+  elements = stripe.elements({ clientSecret })
 
   const paymentElementOptions = {
     layout: "tabs",
-  };
+  }
 
-  const paymentElement = elements.create("payment", paymentElementOptions);
-  paymentElement.mount("#payment-element");
-  
-  const buttonSubmit = document.querySelector('form#payment-form button#submit');
-  buttonSubmit.disabled = false;
-  buttonSubmit.querySelector("#button-text").textContent = "Don de " + amount + "€";
+  const paymentElement = elements.create("payment", paymentElementOptions)
+  paymentElement.mount("#payment-element")
+
+  const buttonSubmit = document.querySelector("form#payment-form button#submit")
+  buttonSubmit.disabled = false
+  buttonSubmit.querySelector("#button-text").textContent =
+    "Don de " + amount + "€"
 }
 
 async function handleSubmit(e) {
-  e.preventDefault();
-  setLoading(true);
+  e.preventDefault()
+  setLoading(true)
 
   const { error } = await stripe.confirmPayment({
     elements,
     confirmParams: {
       return_url: "",
     },
-  });
+  })
 
   // This point will only be reached if there is an immediate error when
   // confirming the payment. Otherwise, your customer will be redirected to
@@ -55,66 +54,66 @@ async function handleSubmit(e) {
   // be redirected to an intermediate site first to authorize the payment, then
   // redirected to the `return_url`.
   if (error.type === "card_error" || error.type === "validation_error") {
-    showMessage(error.message);
+    showMessage(error.message)
   } else {
-    showMessage("An unexpected error occurred.");
+    showMessage("An unexpected error occurred.")
   }
 
-  setLoading(false);
+  setLoading(false)
 }
 
 // Fetches the payment intent status after payment submission
 async function checkStatus() {
   const clientSecret = new URLSearchParams(window.location.search).get(
     "payment_intent_client_secret"
-  );
+  )
 
   if (!clientSecret) {
-    return;
+    return
   }
 
-  const { paymentIntent } = await stripe.retrievePaymentIntent(clientSecret);
+  const { paymentIntent } = await stripe.retrievePaymentIntent(clientSecret)
 
   switch (paymentIntent.status) {
     case "succeeded":
-      showMessage("Payment succeeded!");
-      break;
+      showMessage("Payment succeeded!")
+      break
     case "processing":
-      showMessage("Your payment is processing.");
-      break;
+      showMessage("Your payment is processing.")
+      break
     case "requires_payment_method":
-      showMessage("Your payment was not successful, please try again.");
-      break;
+      showMessage("Your payment was not successful, please try again.")
+      break
     default:
-      showMessage("Something went wrong.");
-      break;
+      showMessage("Something went wrong.")
+      break
   }
 }
 
 // ------- UI helpers -------
 
 function showMessage(messageText) {
-  const messageContainer = document.querySelector("#payment-message");
+  const messageContainer = document.querySelector("#payment-message")
 
-  messageContainer.classList.remove("hidden");
-  messageContainer.textContent = messageText;
+  messageContainer.classList.remove("hidden")
+  messageContainer.textContent = messageText
 
   setTimeout(function () {
-    messageContainer.classList.add("hidden");
-    messageContainer.textContent = "";
-  }, 4000);
+    messageContainer.classList.add("hidden")
+    messageContainer.textContent = ""
+  }, 4000)
 }
 
 // Show a spinner on payment submission
 function setLoading(isLoading) {
   if (isLoading) {
     // Disable the button and show a spinner
-    document.querySelector("#submit").disabled = true;
-    document.querySelector("#spinner").classList.remove("hidden");
-    document.querySelector("#button-text").classList.add("hidden");
+    document.querySelector("#submit").disabled = true
+    document.querySelector("#spinner").classList.remove("hidden")
+    document.querySelector("#button-text").classList.add("hidden")
   } else {
-    document.querySelector("#submit").disabled = false;
-    document.querySelector("#spinner").classList.add("hidden");
-    document.querySelector("#button-text").classList.remove("hidden");
+    document.querySelector("#submit").disabled = false
+    document.querySelector("#spinner").classList.add("hidden")
+    document.querySelector("#button-text").classList.remove("hidden")
   }
 }
